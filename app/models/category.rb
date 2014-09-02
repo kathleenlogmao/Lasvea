d74d830dd79593e4d58a0928174c0fe465634ba2
Raphael Alampay <raphael.alampay@gmail.com>
2014-09-02 18:22:09 +0800

    minor changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ActiveRecord::Base
   has_attached_file :banner, styles: { thumb: "50x50",
                                         medium: "#250x150",
                                         large: "450x300" },
-                              default_url: "http://placehold.it/250x150"
+                              default_url: "http://placehold.it/350x250"
   validates_attachment_content_type :banner, content_type: %w(image/jpeg image/jpg image/png)
 
   validates :name, presence: true
